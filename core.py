609cde1398be85600f8eaf7912e9b954d6a34f80
demch <offer.demchenko@gmail.com>
2023-09-20 19:59:48 +0300

    add limit on the number of button presses

--- core.py
+++ core.py
@@ -1,17 +1,20 @@
 import telebot
 from telebot import types
+import time
 
 from local import *
-from youtube import send_latest_videos
+from youtube import send_latest_videos, send_random_videos
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+button_click_times = {}
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    button1 = types.KeyboardButton("Не лінись і підпишись")
-    button2 = types.KeyboardButton("Нове відео на каналі")
+    button1 = types.KeyboardButton("Щойно вийшло")
+    button2 = types.KeyboardButton("Рандомне відео")
     button3 = types.KeyboardButton("Що нового у автора")
     button4 = types.KeyboardButton("На каву")
     button5 = types.KeyboardButton("Затишний чат без бану і ненависті")
@@ -23,19 +26,29 @@ def start(message):
 @bot.message_handler(func=lambda message: True)
 def handle_buttons(message):
     text = message.text
-    match text:
-        case "Не лінись і підпишись":
-            bot.send_message(message.chat.id, "Ви натиснули Кнопку 1")
-        case "Нове відео на каналі":
-            send_latest_videos(message)
-        case "Що нового у автора":
-            bot.send_message(message.chat.id, f"Дізнайся тут:\n{LIFE_CHANNEL}")
-        case "Затишний чат без бану і ненависті":
-            bot.send_message(message.chat.id, f"Спілкуйся тут:\n{TALK_CHANNEL}")
-        case "На каву":
-            bot.send_message(message.chat.id, f"Закинь тут:\n{DONATE}")
-        case _:
-            bot.send_message(message.chat.id, "Не вдалось розпізнати команду")
+    user_id = message.from_user.id
+
+    last_click_time = button_click_times.get(user_id, 0)
+    current_time = time.time()
+
+    if current_time - last_click_time >= 1:
+        button_click_times[user_id] = current_time
+        match text:
+            case "Щойно вийшло":
+                send_latest_videos(message)
+            case "Рандомне відео":
+                send_random_videos(message)
+            case "Що нового у автора":
+                bot.send_message(message.chat.id, f"Дізнайся тут:\n{LIFE_CHANNEL}")
+            case "Затишний чат без бану і ненависті":
+                bot.send_message(message.chat.id, f"Спілкуйся тут:\n{TALK_CHANNEL}")
+            case "На каву":
+                bot.send_message(message.chat.id, f"Закинь тут:\n{DONATE}")
+            case _:
+                bot.send_message(message.chat.id, "Не вдалось розпізнати команду")
+    else:
+        bot.send_message(message.chat.id, "Будь ласка, зачекайте ще трохи перед наступним натисканням кнопки.")
 
 
-bot.polling(none_stop=True)
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
--- youtube.py
+++ youtube.py
@@ -1,17 +1,18 @@
 from googleapiclient.discovery import build
 import telebot
+import random
 
 from local import *
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-def get_latest_videos():
+def get_videos(max_result):
     youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
     request = youtube.search().list(
         part='snippet',
         channelId=YOUTUBE_CHANNEL_ID,
-        maxResults=1,
+        maxResults=max_result,
         order='date'
     )
     response = request.execute()
@@ -21,12 +22,27 @@ def get_latest_videos():
 
 @bot.message_handler(commands=['latestvideos'])
 def send_latest_videos(message):
-    videos = get_latest_videos()
+    videos = get_videos(max_result=1)
+
     if videos:
         for video in videos:
             video_id = video['id']['videoId']
             video_url = f'https://www.youtube.com/watch?v={video_id}'
             video_title = video['snippet']['title']
             bot.send_message(message.chat.id, f'{video_title}\n{video_url}')
+    else:
+        bot.send_message(message.chat.id, 'На жаль, нові відео відсутні.')
+
+
+@bot.message_handler(commands=['randomvideos'])
+def send_random_videos(message):
+    videos = get_videos(max_result=20)
+
+    if videos:
+        random_video = random.choice(videos)
+        video_id = random_video['id']['videoId']
+        video_url = f'https://www.youtube.com/watch?v={video_id}'
+        video_title = random_video['snippet']['title']
+        bot.send_message(message.chat.id, f'{video_title}\n{video_url}')
     else:
         bot.send_message(message.chat.id, 'На жаль, нові відео відсутні.')
